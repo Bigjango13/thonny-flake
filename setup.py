47bf0c26d0ceabbd87628f097ea8f68871d63689
bigjango13 <58828692+bigjango13@users.noreply.github.com>
2022-02-26 13:05:53 -0500

    Updated version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ThonnyFlake8",
-    version="1.0.0",
+    version="1.0.1",
     author="Bigjango13",
     description="A plugin that adds flake8 to the Thonny python IDE.",
     long_description=long_description,
